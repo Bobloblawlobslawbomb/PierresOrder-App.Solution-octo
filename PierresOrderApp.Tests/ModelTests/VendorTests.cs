ac8f63809cd058e354e689aaa75dd7c412e03d4d
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-05-23 19:37:45 -0700

    add passing Vendor.GetAll test

--- PierresOrderApp.Tests/ModelTests/VendorTests.cs
+++ PierresOrderApp.Tests/ModelTests/VendorTests.cs
@@ -1,16 +1,39 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierresOrderApp.Models;
+using System.Collections.Generic;
+using System;
 
 namespace PierresOrderApp.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
       Vendor newVendor = new Vendor("sampleVendor", "sampleDescription");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsListOfAllVendorObjects_List()
+    {
+      string name01 = "Pac Coast";
+      string name02 = "Charlie's Produce";
+      string desc01 = "Fruit";
+      string desc02 = "Vegetable";
+
+      Vendor newVendor1 = new Vendor(name01, desc01);
+      Vendor newVendor2 = new Vendor(name02, desc02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+
+      List<Vendor> result = Vendor.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
